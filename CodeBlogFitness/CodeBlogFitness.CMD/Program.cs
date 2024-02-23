6cf31efda3724b27c474ffc600ecb50b6f5213c4
Dmitry Tsymbal <casyyycas.yyy@gmail.com>
2024-02-23 19:55:39 +0300

    Создана первоначальная реализация сохранения и загрузки пользователя

--- CodeBlogFitness/CodeBlogFitness.CMD/Program.cs
+++ CodeBlogFitness/CodeBlogFitness.CMD/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using CodeBlogFitness.BL.Controller;
+using CodeBlogFitness.BL.Model;
+using System;
 
 namespace CodeBlogFitness.CMD
 {
@@ -6,6 +8,26 @@ namespace CodeBlogFitness.CMD
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Вас приветствует приложение CodeBlogFitness");
+
+            Console.WriteLine("Введите имя пользователя");
+
+            var name = Console.ReadLine();
+
+            Console.WriteLine("Введите пол: ");
+            var gender = Console.ReadLine();
+
+            Console.WriteLine("Введите дату рождения: ");
+            var birthdate = DateTime.Parse(Console.ReadLine()); // TODO: Переписать
+
+            Console.WriteLine("Введите вес: ");
+            var weight = Double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Введите рост: ");
+            var height = Double.Parse(Console.ReadLine());
+
+            var userController = new UserController(name, gender, birthdate, weight, height);
+            userController.Save();
 
         }
     }
